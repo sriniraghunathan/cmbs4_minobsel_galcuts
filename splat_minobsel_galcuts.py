e5b5c6e61a00bb19662542bd388eaced1f08ad0c
Srinivasan Raghunathan <sraghunathan@MacBook-Pro-2.local>
2021-12-21 19:47:07 -0800

    Updated the way gal cuts are created; Created min_el, fsky, gal_cut and dust_ratio_wrt_baseline table

--- splat_minobsel_galcuts.py
+++ splat_minobsel_galcuts.py
@@ -1,4 +1,30 @@
-def get_lat_based_masks(nside, min_lat = -90., max_lat = 45., delta_lat = 15., rotate_to_radec = True, use_lat_steps = False):
+def get_lat_based_masks(nside, max_lat_mask = 50., delta_lat = 10., rotate_to_radec = True):
+    """
+    #get lat-based masks in ra/dec coordinates
+    """
+    lat_arr = np.arange( delta_lat, max_lat_mask+1., delta_lat)
+
+    hmask_dic = {}
+    npix = H.nside2npix( nside )
+    phi_deg, theta_deg = H.pix2ang( nside, np.arange(npix), lonlat = 1 )
+    
+    for lval in lat_arr:
+        curr_mask = np.ones( npix )
+        unmask_pixels = np.where( (theta_deg>=-lval) & (theta_deg<=lval) )[0]
+        curr_mask[unmask_pixels] = 0.
+
+        if rotate_to_radec:#rotate to ra/dec
+            curr_mask = healpix_rotate_coords(curr_mask, coord = ['G', 'C'])
+
+        '''
+        H.mollview( curr_mask, coord = ['C', 'G'], sub = (2,2,1) ); H.graticule();
+        H.mollview( curr_mask, sub = (2,2,2) ); H.graticule(); show(); #sys.exit()
+        '''
+        hmask_dic[lval] = curr_mask
+
+    return hmask_dic #these are lat masks in ra/dec coordinates
+
+def get_lat_based_masks_v1(nside, min_lat = -90., max_lat = 45., delta_lat = 15., rotate_to_radec = True, use_lat_steps = False):
     """
     #get lat-based masks in ra/dec coordinates
     """
@@ -32,15 +58,15 @@ def get_lat_based_masks(nside, min_lat = -90., max_lat = 45., delta_lat = 15., r
     if use_lat_steps: #finally add all masks together
         curr_mask = np.sum(lat_mask_arr, axis = 0)
         curr_mask[curr_mask>threshold] = 1.
+        curr_mask[curr_mask<threshold] = 0.
         lat_mask_arr.append( curr_mask )
         l1, l2 = lat_arr[0], lat_arr[-1]
         hmask_dic[(l1, l2)] = curr_mask
         
-        #H.mollview( curr_mask ); H.graticule(); show(); sys.exit()
+    #H.mollview( curr_mask ); H.graticule(); show(); sys.exit()
 
     #return np.asarray( lat_mask_arr ) #these are lat masks in ra/dec coordinates
     return hmask_dic #these are lat masks in ra/dec coordinates
-
 def healpix_rotate_coords(hmap, coord):
     """
     coord = ['C', 'G'] to convert a map in radec to galactic coordinates or vice versa.
@@ -67,6 +93,7 @@ def healpix_rotate_coords(hmap, coord):
     if (1):
         rot_hmap = H.smoothing(rot_hmap, fwhm = np.radians(beam_to_use_for_smoothing))
         rot_hmap[rot_hmap>threshold] = 1.
+        rot_hmap[rot_hmap<threshold] = 0.
 
     return rot_hmap
 
@@ -145,7 +172,7 @@ from astropy import units as u
 from astropy import coordinates as coord
 from astropy.coordinates import SkyCoord, EarthLocation
 
-use_lat_steps = False #True
+#use_lat_steps = False #True #False
 beam_to_use_for_smoothing = 0.5 #some rough smoothing
 threshold = 0.001
 nside = 512
@@ -154,7 +181,7 @@ dust_145ghz_fname = 'data/cmbs4_dust_uKCMB_LAT-MFL2_nside512_dust_0000.fits'
 dust_145ghz = H.read_map(dust_145ghz_fname, field = (0, 1, 2))
 #dust_145ghz = H.ud_grade(dust_145ghz, nside)
 
-lat_mask_dic = get_lat_based_masks(nside, use_lat_steps = use_lat_steps)
+lat_mask_dic = get_lat_based_masks(nside)#, use_lat_steps = use_lat_steps)
 min_obs_el_err = [20., 25., 30., 35., 40.]
 op_dic = {}
 op_dic['hit_masks'] = {}
@@ -166,7 +193,7 @@ for min_obs_el in min_obs_el_err:
     op_dic['hit_masks'][min_obs_el] = hmask
     fsky1 = len( np.where(hmask>0.)[0] )/len(hmask) #all inds with more than xx per cent hit    
     fsky = np.sum( hmask[hmask>0.] )/len(hmask) #all inds with more than xx per cent hit    
-    #print(beam_to_use_for_smoothing, min_obs_el, fsky1, fsky, np.mean(hmask)); sys.exit()
+    print(beam_to_use_for_smoothing, min_obs_el, fsky1, fsky, np.mean(hmask)); #sys.exit()
 
     '''
     H.mollview(hmask, min = 0., max = 1., coord = ['C', 'G'], sub=(2,3,1)); H.graticule(); title(r'Min. el = %g; fsky = %.3f' %(min_obs_el, fsky)); 
@@ -178,9 +205,10 @@ for min_obs_el in min_obs_el_err:
     H.mollview(hmask * lat_mask_dic[(-90.0, 45.0)], min = 0., max = 1., sub=(2,3,6)); H.graticule(); title(r'Min. el = %g; fsky = %.3f' %(min_obs_el, fsky)); show(); sys.exit()
     '''
 
-    for (l1, l2) in lat_mask_dic:
-        print('\t(l1, l2) = (%g, %g)' %(l1, l2))
-        lat_mask = lat_mask_dic[(l1, l2)]
+    #for (l1, l2) in lat_mask_dic:
+        #lat_mask = lat_mask_dic[(l1, l2)]
+    for bval in lat_mask_dic:
+        lat_mask = lat_mask_dic[bval]
         '''
         fsky = np.sum( np.where(hmask>0.)[0] )/len(hmask) #all inds with more than xx per cent hit    
         H.mollview(hmask, min = 0., max = 1., sub = (1,3,1)); H.graticule(verbose = False); title(r'Min. el = %g; fsky = %.3f' %(min_obs_el, fsky));
@@ -192,20 +220,28 @@ for min_obs_el in min_obs_el_err:
         H.mollview(curr_dust_map); H.graticule(verbose = False); show()
         '''
         hmask_with_gal_cut = hmask * lat_mask
+        hmask_with_gal_cut[hmask_with_gal_cut<threshold] = 0.
         #fsky = len( np.where(hmask_with_gal_cut>0.)[0] )/len(hmask_with_gal_cut) #all inds with more than xx per cent hit
         fsky = np.sum( hmask_with_gal_cut[hmask_with_gal_cut>0.])/len(hmask_with_gal_cut) #all inds with more than xx per cent hit
+
+        #print('\t(l1, l2) = (%g, %g) and fsky = %.3f' %(l1, l2, fsky))
+        print('\tLast mask = +/- %g deg. and fsky = %.3f' %(bval, fsky))
         
         curr_mask = hmask * lat_mask 
         curr_dust_map = curr_mask * np.copy( dust_145ghz )
         cl = H.anafast(curr_dust_map, lmax = lmax)
         cl = cl/fsky
-        op_dic['cl'][min_obs_el][(l1,l2)] = cl
+        #op_dic['cl'][min_obs_el][(l1,l2)] = cl
+        op_dic['cl'][min_obs_el][bval] = cl
 
 op_dic['dust_145ghz'] = dust_145ghz
+'''
 if not use_lat_steps:
     opfname = 'results/splat_hitmask_galmask_galdustcl.npy'
 else:
     opfname = 'results/splat_hitmask_galmask_galdustcl_latsteps.npy'
+'''
+opfname = 'results/splat_hitmask_galmask_galdustcl.npy'
 np.save(opfname, op_dic)
 sys.exit()
 
